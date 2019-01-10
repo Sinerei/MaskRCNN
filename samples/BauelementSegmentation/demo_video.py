4fb1489b817ad1582c6675b18b8751228678b421
Sinerei <41483944+Sinerei@users.noreply.github.com>
2019-01-10 16:04:44 +0100

    Update demo_video.py

--- samples/BauelementSegmentation/demo_video.py
+++ samples/BauelementSegmentation/demo_video.py
@@ -23,7 +23,7 @@ from samples.bauelement import Bauelement
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
 # Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "logs/bauelement20190107T1028/mask_rcnn_bauelement_0010.h5")
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "logs/bauelement20190107T1028/mask_rcnn_bauelement_0030.h5")
 # Download COCO trained weights from Releases if needed
 if not os.path.exists(COCO_MODEL_PATH):
     utils.download_trained_weights(COCO_MODEL_PATH)
