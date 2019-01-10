8a0545a398aee8819fde818a315eaabbac7a60bc
Sinerei <41483944+Sinerei@users.noreply.github.com>
2019-01-10 16:02:42 +0100

    Update demo_video.py

--- samples/BauelementSegmentation/demo_video.py
+++ samples/BauelementSegmentation/demo_video.py
@@ -1,19 +1,54 @@
 
 import cv2
-import numpy as np
-from mrcnn.visualize_video import model, display_instances, class_names
-
+from mrcnn.visualize_video import display_instances
+import os
+import sys
 
 VIDEO_NAME = 'kondensators.mp4'
 
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
 
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+import mrcnn.model as modellib
+
+
+# Import COCO config
+sys.path.append(os.path.join(ROOT_DIR, "samples/bauelement/"))  # To find local version
+from samples.bauelement import Bauelement
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "logs/bauelement20190107T1028/mask_rcnn_bauelement_0010.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+
+class InferenceConfig(Bauelement.BauelementConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+
+config = InferenceConfig()
+config.display()
+
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+class_names = ['BG', 'Kondensator', 'Widerstand']
+
+# Detection
 capture = cv2.VideoCapture(VIDEO_NAME)
-size = (
-    int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)),
-    int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-)
+size = (int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
 codec = cv2.VideoWriter_fourcc(*'DIVX')
-output = cv2.VideoWriter('videofile_masked2.avi', codec, 10.0, size)
+output = cv2.VideoWriter('masked_video.avi', codec, 20.0, size)
 
 # i = 0
 # frame_rate_divider = 3
